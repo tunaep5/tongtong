0c6af2eeb5d52c7b51f2612c99f9e4d0f9a7cd83
sunyoung <sunnyk5780@gmail.com>
2020-06-08 02:34:26 +0900

    Component 형태로  변경

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      Hello, Tong Tong!
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Hello, Tong Tong!
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
