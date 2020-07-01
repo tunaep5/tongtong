bf790e052d8fef7fa0d8bd954218dfd3db811443
Sunny.K <sunnyk5780@gmail.com>
2020-07-01 21:02:45 +0900

    Create App.css

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Character from './components/Character'
 import Todo from './components/Todo'
 import About from './components/About'
+import './styles/App.css'
 
 class App extends Component {
   render() {
